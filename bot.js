7cfe4bf315a03459cff9c3add61e7d412af58262
Yuri <yur0n@yahoo.com>
2024-08-20 19:10:42 +0300

    up

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ export const bot = new Bot(process.env.BOT);
 bot.command("start", (ctx) => ctx.reply("Welcome!"));
 
 bot.on("message", (ctx) => {
-	const [username, token] = ctx.msg.split(' ')
+	const [username, token] = ctx.msg.text.split(' ')
 	const user = User.findOne({ username })
 	if (!user) return ctx.reply('User not found')
 	user.token = token
